7495770dfac2539cb52725c5770214168df1de44
ITDevRene <43797984+ITDevRene@users.noreply.github.com>
2020-12-13 10:22:27 -0500

    All pages added

---
+++ spirits_page/main.js
@@ -0,0 +1,10 @@
+
+function loadImage() {
+    
+    document.getElementsByClassName("images");
+    
+    var imgPath = require('path');
+    
+    var filename = imgPath.basename();
+    return "Hello World";
+}
